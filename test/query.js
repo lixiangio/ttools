08fd15a695511bd3c7cbce5989a2b1de3a01600b
LiXiang <xianglqqo@gmail.com>
2018-09-10 10:47:06 +0800

    修复query选择器精确匹配错误

--- test/query.js
+++ test/query.js
@@ -1,47 +1,90 @@
 const test = require('jtf')
 const T = require('..')
 
-test('精确查询', t => {
-
-   let data = {
-      "push": {
-         enable: true
-      },
-      "sms": {
-         enable: false
-      },
-      "user": {
-         enable: true
-      }
+let data = {
+   "push": {
+      enable: true
+   },
+   "sms": {
+      enable: false
+   },
+   "user": {
+      enable: true
    }
+}
 
-   let result = T(data).query({ '*.enable': true })
 
-   t.deepEqual({ push: { enable: true }, user: { enable: true } }, result)
+test('精确匹配', t => {
+
+   let result = T(data).query({ 'user.enable': true })
+
+   t.deepEqual({ user: { enable: true } }, result)
 
 })
 
 
-test.skip('模糊查询', t => {
+test('模糊匹配', t => {
 
-   let data = {
-      "push": {
-         enable: false
+   let result = T(data).query({ '*.enable': false })
+
+   // console.log(result)
+
+   t.deepEqual({ sms: { enable: false } }, result)
+
+})
+
+
+test('回调函数，Object', t => {
+
+
+   let result = T(data).query({ '*.enable': false }, function (key, value) {
+
+      t.deepEqual('sms', key)
+
+      t.deepEqual({ enable: false }, value)
+
+   })
+
+   t.deepEqual({ sms: { enable: false } }, result)
+
+})
+
+
+test('回调函数，Array', t => {
+
+   let data = [
+      {
+         id: 555,
+         cid: 15,
+         oo: {
+            o1: 34,
+            o2: 56
+         }
       },
-      "sms": {
-         enable: true
+      {
+         id: 343,
+         cid: 566,
+         oo: {
+            o1: 999,
+            o2: 1111
+         }
       },
-      "user": {
-         enable: false
-      }
-   }
+      {
+         id: 343,
+         cid: 566,
+         oo: {
+            o1: 34,
+            o2: 1111
+         }
+      },
+   ]
 
-   let result = T(data).query({ '*.enable': true })
+   let result = T(data).query({ '*.oo.o1': 999 }, function (item) {
 
-   console.log(result)
+      t.deepEqual({ id: 343, cid: 566, oo: { o1: 999, o2: 1111 } }, item)
 
-   t.deepEqual([
-      { enable: true }
-   ], result)
+   })
+
+   t.deepEqual([{ id: 343, cid: 566, oo: { o1: 999, o2: 1111 } }], result)
 
 })
